33122385ed78bc07e61e7d425b912bfc71dab04f
fahust <kevin.dellova@hotmail.fr>
2022-11-12 20:29:37 +0100

    controller user message

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,6 +2,13 @@ import { NextFunction, Request, Response } from 'express';
 import { CreateUserDto } from '@dtos/users.dto';
 import { User } from '@interfaces/users.interface';
 import userService from '@services/users.service';
+import {
+  CONTROLLER_CREATED_USER_SUCCES_MESSAGE,
+  CONTROLLER_DELETED_USER_SUCCES_MESSAGE,
+  CONTROLLER_FIND_ALL_USER_SUCCES_MESSAGE,
+  CONTROLLER_FIND_ONE_USER_SUCCES_MESSAGE,
+  CONTROLLER_UPDATED_USER_SUCCES_MESSAGE,
+} from '@/messages/errors';
 
 class UsersController {
   public userService = new userService();
@@ -10,7 +17,7 @@ class UsersController {
     try {
       const findAllUsersData: User[] = await this.userService.findAllUser();
 
-      res.status(200).json({ data: findAllUsersData, message: 'findAll' });
+      res.status(200).json({ data: findAllUsersData, message: CONTROLLER_FIND_ALL_USER_SUCCES_MESSAGE });
     } catch (error) {
       next(error);
     }
@@ -21,7 +28,7 @@ class UsersController {
       const userId: string = req.params.id;
       const findOneUserData: User = await this.userService.findUserById(userId);
 
-      res.status(200).json({ data: findOneUserData, message: 'findOne' });
+      res.status(200).json({ data: findOneUserData, message: CONTROLLER_FIND_ONE_USER_SUCCES_MESSAGE });
     } catch (error) {
       next(error);
     }
@@ -32,7 +39,7 @@ class UsersController {
       const userData: CreateUserDto = req.body;
       const createUserData: User = await this.userService.createUser(userData);
 
-      res.status(201).json({ data: createUserData, message: 'created' });
+      res.status(201).json({ data: createUserData, message: CONTROLLER_CREATED_USER_SUCCES_MESSAGE });
     } catch (error) {
       next(error);
     }
@@ -44,7 +51,7 @@ class UsersController {
       const userData: CreateUserDto = req.body;
       const updateUserData: User = await this.userService.updateUser(userId, userData);
 
-      res.status(200).json({ data: updateUserData, message: 'updated' });
+      res.status(200).json({ data: updateUserData, message: CONTROLLER_UPDATED_USER_SUCCES_MESSAGE });
     } catch (error) {
       next(error);
     }
@@ -55,7 +62,7 @@ class UsersController {
       const userId: string = req.params.id;
       const deleteUserData: User = await this.userService.deleteUser(userId);
 
-      res.status(200).json({ data: deleteUserData, message: 'deleted' });
+      res.status(200).json({ data: deleteUserData, message: CONTROLLER_DELETED_USER_SUCCES_MESSAGE });
     } catch (error) {
       next(error);
     }
--- src/messages/errors.ts
+++ src/messages/errors.ts
@@ -1,3 +1,9 @@
 export const CONTROLLER_SIGNUP_SUCCES_MESSAGE = "registration successful";
 export const CONTROLLER_LOGIN_SUCCES_MESSAGE = "login successful";
 export const CONTROLLER_LOGOUT_SUCCES_MESSAGE = "logout successful";
+
+export const CONTROLLER_FIND_ALL_USER_SUCCES_MESSAGE = "all users founded";
+export const CONTROLLER_FIND_ONE_USER_SUCCES_MESSAGE = "one user founded";
+export const CONTROLLER_CREATED_USER_SUCCES_MESSAGE = "user created";
+export const CONTROLLER_UPDATED_USER_SUCCES_MESSAGE = "user updated";
+export const CONTROLLER_DELETED_USER_SUCCES_MESSAGE = "user deleted";
