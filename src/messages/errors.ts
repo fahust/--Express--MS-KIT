bb8d905c00093a054a8699ccb9d7ed7dc3bfae92
fahust <kevin.dellova@hotmail.fr>
2022-11-12 20:24:32 +0100

    controller authentication message

---
+++ src/messages/errors.ts
@@ -0,0 +1,3 @@
+export const CONTROLLER_SIGNUP_SUCCES_MESSAGE = "registration successful";
+export const CONTROLLER_LOGIN_SUCCES_MESSAGE = "login successful";
+export const CONTROLLER_LOGOUT_SUCCES_MESSAGE = "logout successful";
