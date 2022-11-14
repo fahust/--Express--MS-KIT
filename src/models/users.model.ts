656c6625dea227484214542481eed582beee3e9f
fahust <kevin.dellova@hotmail.fr>
2022-11-14 01:19:18 +0100

    schema model

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -7,10 +7,57 @@ const userSchema: Schema = new Schema(
       type: String,
       required: true,
       unique: true,
+      trim: true,
     },
     password: {
       type: String,
       required: true,
+      minlength: 4,
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      required: false,
+      ref: 'Product',
+    },
+    locale: {
+      type: String,
+      required: false,
+      minlength: 4,
+    },
+    thumbnail: {
+      type: String,
+      required: false,
+      minlength: 4,
+    },
+    firstName: {
+      type: String,
+      required: false,
+      minlength: 4,
+    },
+    lastName: {
+      type: String,
+      required: false,
+      minlength: 4,
+    },
+    userName: {
+      type: String,
+      required: false,
+      minlength: 4,
+    },
+    phones: {
+      type: [String],
+      required: false,
+      default: [],
+    },
+    hasNewsletter: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    permissions: {
+      type: [String],
+      required: false,
+      default: [],
     },
   },
   {
