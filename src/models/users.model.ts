b1380d70fce35719766ac3d7368a1e7a41a528c2
fahust <kevin.dellova@hotmail.fr>
2022-11-14 01:04:00 +0100

    time stamp mongdb user

--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -1,17 +1,22 @@
 import { model, Schema, Document } from 'mongoose';
 import { User } from '@interfaces/users.interface';
 
-const userSchema: Schema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema: Schema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-});
+);
 
 const userModel = model<User & Document>('User', userSchema);
 
